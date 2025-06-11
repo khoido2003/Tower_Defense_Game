8528fd8ed11e563d5a60624dbb3bddc45eb191fe
khoido2003 <khoizpro31@gmail.com>
2025-06-11 23:35:34 +0700

    Add place building and building type

---
+++ Assets/Scripts/BuildingManager.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class BuildingManager : MonoBehaviour
+{
+    private Camera mainCamera;
+
+    [SerializeField]
+    private BuildingTypeSO buildingType;
+
+    private BuildingTypeListSO buildingTypeList;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+
+        // Debug.Log(Resources.Load<BuildingTypeListSO>("BuildingTypeList"));
+
+        buildingTypeList = Resources.Load<BuildingTypeListSO>(typeof(BuildingTypeListSO).Name);
+
+        buildingType = buildingTypeList.list[0];
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(buildingType.prefab, GetMouseWorldPosition(), Quaternion.identity);
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            buildingType = buildingTypeList.list[0];
+        }
+
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            buildingType = buildingTypeList.list[1];
+        }
+    }
+
+    private Vector3 GetMouseWorldPosition()
+    {
+        Vector3 mouseWorldPosition = (mainCamera.ScreenToWorldPoint(Input.mousePosition));
+
+        mouseWorldPosition.z = 0f;
+        return mouseWorldPosition;
+    }
+}
